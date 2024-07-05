799c562ee09dbbfdf3124558ae91255e4fd56031
Gui Silva <adrianon10@gmail.com>
2024-07-05 05:02:56 -0300

    feat: add products fetch from stripe

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
+/* eslint-disable prettier/prettier */
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "files.stripe.com",
+        pathname: "**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
-export default function Home() {
+import 'keen-slider/keen-slider.min.css'
+
+import Stripe from 'stripe'
+
+import { Header } from '@/components/header'
+import { Products } from '@/components/products'
+import { stripe } from '@/lib/stripe'
+
+export default async function Home() {
+  const response = await stripe.products.list({
+    expand: ['data.default_price'],
+  })
+
+  const products = response.data.map((product) => {
+    const price = product.default_price as Stripe.Price
+    return {
+      id: product.id,
+      name: product.name,
+      image_url: product.images[0],
+      price: price.unit_amount! / 100,
+    }
+  })
+
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div className="flex h-screen flex-col bg-gray-900 pb-6">
+      <Header />
+      <main className="ml-auto flex min-h-[656px] w-full max-w-[calc(100vw-((100vw-1180px)/2))]">
+        <Products products={products} />
+      </main>
+    </div>
   )
 }
