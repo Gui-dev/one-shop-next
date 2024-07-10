66b297c91ed2d0d4ddf7864ff91e8715130f7c50
Gui Silva <adrianon10@gmail.com>
2024-07-10 04:26:13 -0300

    feat: add success page

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -4,7 +4,15 @@ import { stripe } from '@/lib/stripe'
 
 export async function POST(request: NextRequest) {
   const { price_id } = await request.json()
-  const success_url = `${process.env.NEXT_URL}/success`
+
+  if (!price_id) {
+    return Response.json({
+      status: 400,
+      error: 'Price not found',
+    })
+  }
+
+  const success_url = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`
   const cancel_url = `${process.env.NEXT_URL}/cancel`
   const checkoutSession = await stripe.checkout.sessions.create({
     mode: 'payment',
--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,7 +1,52 @@
-export default function Success() {
+import Image from 'next/image'
+import Link from 'next/link'
+import Stripe from 'stripe'
+
+import { stripe } from '@/lib/stripe'
+
+interface ISuccessParams {
+  searchParams: {
+    session_id: string
+  }
+}
+
+export default async function Success({ searchParams }: ISuccessParams) {
+  const { session_id } = searchParams
+  const session = await stripe.checkout.sessions.retrieve(session_id, {
+    expand: ['line_items', 'line_items.data.price.product'],
+  })
+
+  const purchaseDetails = {
+    customerName: session.customer_details?.name,
+    product: session.line_items?.data[0].price!.product as Stripe.Product,
+  }
+
   return (
-    <>
-      <h1>Success</h1>
-    </>
+    <div className="flex flex-col items-center justify-center gap-8">
+      <h1 className="text-2xl text-gray-200">Compra efetuada com sucesso!</h1>
+
+      <div className="flex h-[145px] w-full max-w-[130px] items-center justify-center rounded-lg bg-[linear-gradient(180deg,_#1EA483_0%,_#7465D4_100%)] p-1">
+        <Image
+          src={purchaseDetails.product.images[0]}
+          alt={purchaseDetails.product.name}
+          height={110}
+          width={120}
+          className="object-contain"
+        />
+      </div>
+
+      <p className="max-w-[560px] text-center text-base leading-6 text-gray-300">
+        Olá, <strong>{purchaseDetails.customerName}</strong>, sua{' '}
+        <strong>{purchaseDetails.product.name}</strong> já está a caminho da sua
+        casa
+      </p>
+
+      <Link
+        href="/"
+        className="mt-16 text-sm text-green-500 hover:text-green-500/90"
+      >
+        Voltar ao catálago
+      </Link>
+    </div>
   )
 }
